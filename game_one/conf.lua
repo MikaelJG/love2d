ab9ac117e33675db56f6c714fd677760d20ea794
MikaelJG <mjgonsalves@protonmail.com>
2023-01-17 17:57:26 -0500

    height and size

--- game_one/conf.lua
+++ game_one/conf.lua
@@ -1,8 +1,16 @@
 function love.conf(t)
     -- t.identity = "data/saves" -- creates folder to save data 
-    t.version = "1.0.0"
-    t.console = true 
-    t.externalstorage = true 
-    t.gammacorrect = true 
+    -- t.version = "1.0.0"
+    -- t.console = true 
+    -- t.externalstorage = true 
+    -- t.gammacorrect = true 
+    -- t.audio.mic = true 
+
+    t.window.title = "cool game"
+    t.window.icon = "icon/game_icon.jpg"
+    t.window.width = 1200 
+    t.window.height = 700
+    t.window.resizable = true
+
 
 end
