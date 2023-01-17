b944782c5cd236eee262548bf26db509587c4a8c
MikaelJG <mjgonsalves@protonmail.com>
2023-01-17 18:03:44 -0500

    conf.lua

--- game_one/conf.lua
+++ game_one/conf.lua
@@ -9,8 +9,20 @@ function love.conf(t)
     t.window.title = "cool game"
     t.window.icon = "icon/game_icon.jpg"
     t.window.width = 1200 
+    t.window.minwidth = 1000 
     t.window.height = 700
+    t.window.minheight = 500 
     t.window.resizable = true
+
+    t.window.boderless = true
+    t.window.fullscreen = true
+    t.window.vsync = 1 
+    t.window.display = 2 
+    t.window.x = 100 -- di 
+    t.window.y = 100
+
+
+    
 
 
 end
