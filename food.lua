4532df7815004301bb9d2d057ab5a0f180c0c256
MikaelJG <mjgonsalves@protonmail.com>
2023-01-18 09:04:45 -0500

    eat the food

---
+++ food.lua
@@ -0,0 +1,31 @@
+_G.love = require("love")
+
+function love.load()
+    love.fraphics.setBarckgroundColor(0.5, 0.5, 1)
+
+    _G.pacman = {}
+    pacman.x = 200
+    pacman.y = 250
+    pacman.eat = true
+
+    _G.food_x = 600
+    _G.food_eaten = false 
+end
+
+function love.update(dt)
+    pacman.x = pacman.x + 1
+
+    if pacman.x >= food_x + 20 then
+        _G.food_eaten = true
+    end
+end
+    
+function love.draw()
+    if not food.eaten then
+        love.graphics.setColor(0, 0, 0)
+
+    -- if pacman hasn't eaten it, the food x position is here
+        love.graphics.rectangle("fill", food_x, 200, 70, 70)
+    end
+end
+
