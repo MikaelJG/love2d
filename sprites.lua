9412064b8311a42cc5ff3bae42d1eef65077df51
MikaelJG <mjgonsalves@protonmail.com>
2023-01-18 10:06:53 -0500

    sprites first

---
+++ sprites.lua
@@ -0,0 +1,23 @@
+_G.love = require("love")
+
+function love.load()
+    jack = {
+        x = 0,
+        y = 0
+        sprite = love.graphics.newImage("sprites/spritesheet.png")
+    }
+
+    -- write image size. ex 5300x570 pixels
+    -- fancy way to set var
+    sprite_width, sprite_height = 5352, 570
+
+    
+end
+
+function love.load()
+
+end
+
+function love.load()
+
+end
