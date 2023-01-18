acd176bf652b8855c3bd798bee202d656769222a
MikaelJG <mjgonsalves@protonmail.com>
2023-01-18 08:37:21 -0500

    shapes

---
+++ draw.lua
@@ -0,0 +1,17 @@
+-- in config file
+
+function love.draw()
+    love.graphics.rectangle("fill", 50, 50, 50, 50)
+    love.graphics.rectangle("line", 50, 50, 50, 50)
+    love.graphics.rectangle("line", 10, 500, 50, 50)
+    -- love.graphics.rectangle("#", y , x , width, height)
+
+end
+
+____________ x (axes)
+|
+|
+|
+|
+|
+y
