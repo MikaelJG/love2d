4b0f4dcdf921d58ea148a2f27a4c6356f9680a2d
MikaelJG <mjgonsalves@protonmail.com>
2023-01-18 08:48:53 -0500

    pacman

--- draw.lua
+++ draw.lua
@@ -8,8 +8,21 @@ function love.draw()
 
     --- RGB (red, green, blue system)
 
-   love.graphics.setColor(0, 0, 0)
-   love.graphics.setColor(75 / 255, 148 / 255, 10 / 255)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.setColor(75 / 255, 148 / 255, 10 / 255)
+
+    love.graphics.circle("line", 100, 100, 50)
+
+    love.graphics.setColor(1, 0.7, 0.1)
+
+    -- Pacman shape (cut circle)
+    love.graphics.arc("line", 200, 250, 60, 0)
+
+    -- Pacman
+
+    love.graphics.setColor(1, 0.7, 0.1)
+    love.graphics.arc("line", 200, 250, 60, 1, 5)
+
 
     
 
