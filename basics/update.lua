93fa39fe39f27668bba56a4a5e553651ea0d2eda
MikaelJG <mjgonsalves@protonmail.com>
2023-01-22 20:48:08 -0500

    moving

---
+++ basics/update.lua
@@ -0,0 +1,10 @@
+function love.update(dt)
+
+-- any code that goes into update, 
+-- will be run every single frame
+
+-- dt is downtime?
+
+dt
+The most common shorthand for delta-time,
+passed to love.update to represent the amount of time which has passed
---
+++ characters/player.lua
@@ -0,0 +1,5 @@
+player = {}
+player.x = 0
+player.y = 0
+
+love.graphics.draw(sprites.link, payer.x, player.y)
