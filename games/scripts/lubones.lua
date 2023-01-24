e89fe67effbcd7c236d0a59c077e84eb83399fe2
MikaelJG <mjgonsalves@protonmail.com>
2023-01-24 13:24:25 -0500

    creating a game with menu

--- games/scripts/lubones.lua
+++ games/scripts/lubones.lua
@@ -1,17 +1,23 @@
 -- get arguments
+-- a name should be given.
 
--- touch conf.lua file
 
+-- mkdir components objects states
+-- touch conf.lua main.lua globals.lua ./components/Button.lua ./components/Test.lua ./objects/Player.lua ./states/Game.lua Menu.lua
+
+os.execute("mkdir components")
+
+
+-- conf.lua
 -- looks like this
 
         -- local love = require "love"
         -- 
         -- function love.conf(app)
         --     app.window.width = 1280
-        --     app.window.heihgt = 720
+        --     app.window.height = 720
         --     app.window.title = argument_two
         -- end
 
--- touche main.lua file
 
 
--- games/scripts/menu/states/Game.lua
+++ games/scripts/menu/states/Game.lua
@@ -41,12 +41,12 @@ function Game()
         startNewGame = function (self, player)
             self:changeGameState("running")
         
-            asteroids = {}
-        
-            local as_x = math.floor(math.random(love.graphics.getWidth()))
-            local as_y = math.floor(math.random(love.graphics.getHeight()))
-    
-            table.insert(asteroids, 1, Asteroids(as_x, as_y, 100, self.level, true))
+            -- ON START, spawn an enemy or player, like asteroids object here
+
+            -- asteroids = {}
+            -- local as_x = math.floor(math.random(love.graphics.getWidth()))
+            -- local as_y = math.floor(math.random(love.graphics.getHeight()))
+            -- table.insert(asteroids, 1, Asteroids(as_x, as_y, 100, self.level, true))
         end
     }
 end
