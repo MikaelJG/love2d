577be0ac8a7bf114ac6152edc08e2e4edf6c89a0
MikaelJG <mjgonsalves@protonmail.com>
2023-01-24 16:13:40 -0500

    dream creates a game with a menu, pause

--- dream/dream.lua
+++ dream/dream.lua
@@ -9,24 +9,12 @@ local fileCreate = "touch conf.lua main.lua globals.lua ./components/Button.lua 
 os.execute(folderCreate)
 os.execute(fileCreate)
 
-function dirLookup(dir)
-   local p = io.popen('find "'..dir..'" -type f')  --Open directory look for files, save data in p. By giving '-type f' as parameter, it returns all files.     
-      for file in p:lines() do                         --Loop through all files
-        print(file)       
-      end
-end
+-- find .sh files, if we have to
 
-
--- conf.lua
--- looks like this
-
-        -- local love = require "love"
-        -- 
-        -- function love.conf(app)
-        --     app.window.width = 1280
-        --     app.window.height = 720
-        --     app.window.title = argument_two
-        -- end
-
-
+-- function dirLookup(dir)
+--    local p = io.popen('find "'..dir..'" -type f')  --Open directory look for files, save data in p. By giving '-type f' as parameter, it returns all files.     
+--       for file in p:lines() do                         --Loop through all files
+--         print(file)       
+--       end
+-- end
 
