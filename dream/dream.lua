7c885094def8282f9c5b46fc9615cd6d57912dc1
MikaelJG <mjgonsalves@protonmail.com>
2023-01-25 08:53:24 -0500

    finding files with lua

--- dream/dream.lua
+++ dream/dream.lua
@@ -12,11 +12,9 @@
 -- find .sh files, if we have to
 
 local p = io.popen('find ./sh_writers -type f')
-print(p[1])
-print(p[2])
-       for file in p:lines() do                         --Loop through all files
-         print(file)       
-       end
+for file in p:lines() do                         --Loop through all files
+    print(file)       
+end
 
 -- function dirLookup(dir)
 --    local p = io.popen('find "'..dir..'" -type f')  --Open directory look for files, save data in p. By giving '-type f' as parameter, it returns all files.     
